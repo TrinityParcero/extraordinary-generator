fcafae78c2cd74749bc9d21f6abdbb7a2396ecec
Trin <razyokatt@gmail.com>
2021-10-16 22:01:29 -0400

    fixes for styles in trinket page

--- src/components/pages/Trinket.jsx
+++ src/components/pages/Trinket.jsx
@@ -6,21 +6,20 @@ import { generateTrinkets } from '../../trinketLogic';
 function Trinkets() {
   return (
     <main id="trinket">
+      <div id="leftChunk">
         <CheckboxFieldset id="sources" legend="Source Material" name="source" selectAll={true} deselectAll={true}
-        values={["Player's Handbook", "Elemental Evil", "Curse of Strahd", "Lost Laboratory of Kwalish"]}/>
-        
-
-        <div id="gen">
-            <div id="num">
-                <p id="howMany">how many trinkets?</p>
-                <NumToggle/>
-            </div>
-
-            <div id="generator">
-                <p id="generated"></p>
-                <button onClick={generateTrinkets} id="genButton">generate</button>
-            </div>
+          values={["Player's Handbook", "Elemental Evil", "Curse of Strahd", "Lost Laboratory of Kwalish"]}/>
+        <div id="num">
+          <p id="howMany">how many trinkets?</p>
+          <NumToggle/>
         </div>
+      </div>
+      <div id="gen">
+        <div id="generator">
+              <p id="generated"></p>
+              <button onClick={generateTrinkets} id="genButton">generate</button>
+        </div>
+      </div>
     </main>
   );
 }
